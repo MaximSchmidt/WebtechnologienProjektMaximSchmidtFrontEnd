7a3f53977cb5064bb5fa86f7473abd0b89b79d9b
MaximSchmidt <Maxim.Schmidt@student.htw-berlin.de>
2022-12-17 20:30:06 +0100

    Meilenstein 4: Backend kann das Frontend erreichen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
+  <h1>ToDos</h1>
+  <div class="container-fluid">
+    <to-do-list-table></to-do-list-table>
+  </div>
+  <to-do-list-create-form></to-do-list-create-form>
 </template>
 
 <script>
-
+import ToDoListTable from '@/components/ToDoListTable'
+import ToDoListCreateForm from '@/components/ToDoListCreateForm'
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Home'
+  name: 'ToDos',
+  components: {
+    ToDoListCreateForm,
+    ToDoListTable
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch('http://localhost:8080/api/v1/todolist', requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error))
+  }
 }
-
 </script>
-
 <style scoped>
 </style>
