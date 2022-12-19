ac7a3600d3a0b4398afdb961ce862ada147c4eb4
MaximSchmidt <Maxim.Schmidt@student.htw-berlin.de>
2022-12-19 22:23:45 +0100

    Frontend zeigt Daten aus Datenbank an

--- src/components/ToDoListTable.vue
+++ src/components/ToDoListTable.vue
@@ -1,9 +1,52 @@
 <template>
+  <table class="table table-dark">
+    <thead class="thead-dark">
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">titel</th>
+      <th scope="col">description</th>
+      <th scope="col">category</th>
+      <th scope="col">date</th>
+      <th scope="col">priority</th>
+      <th scope="col">complete</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="todo in todos" :key="todo.id">
+      <th scope="row">{{todo.id}}</th>
+      <td>{{todo.titel}}</td>
+      <td>{{todo.description}}</td>
+      <td>{{todo.category}}</td>
+      <td>{{todo.date}}</td>
+      <td>{{todo.priority ? 'erledigt' : 'nicht erledigt'}}</td>
+      <td>{{todo.complete ? 'erledigt' : 'nicht erledigt'}}</td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-  name: 'ToDoListTable'
+  name: 'ToDoListTable',
+  data () {
+    return {
+      todos: []
+    }
+  },
+  mounted () {
+    const endpoint = 'http://localhost:8080/api/v1/todolist'
+    console.log(endpoint)
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(todo => {
+        this.todos.push(todo)
+      }))
+      .then(error => console.log('error', error))
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,16 @@
 <template>
   <h1>ToDos</h1>
   <div class="container-fluid">
-    <to-do-list-table></to-do-list-table>
+    <ToDoListTable> </ToDoListTable>
   </div>
-  <to-do-list-create-form></to-do-list-create-form>
 </template>
 
 <script>
 import ToDoListTable from '@/components/ToDoListTable'
-import ToDoListCreateForm from '@/components/ToDoListCreateForm'
 export default {
   name: 'ToDos',
   components: {
-    ToDoListCreateForm,
     ToDoListTable
-  },
-  data () {
-    return {
-    }
-  },
-  methods: {
-  },
-  mounted () {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    }
-    fetch('http://localhost:8080/api/v1/todolist', requestOptions)
-      .then(response => response.json())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error))
   }
 }
 </script>
